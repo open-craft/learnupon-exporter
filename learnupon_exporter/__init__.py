18b8c3fa39fa4c80996bc7a30e2408b25e8fad6a
Keith Grootboom <keith@opencraft.com>
2022-05-20 08:28:24 +0200

    feat: filters for email and course enrollment date
    - remove started status
    - fixes loading settings from lms.yml and studio.yml

--- learnupon_exporter/__init__.py
+++ learnupon_exporter/__init__.py
@@ -4,6 +4,6 @@ Django plugin application for exporting edX data to LearnUpon
 
 from __future__ import absolute_import, unicode_literals
 
-__version__ = '0.0.2'
+__version__ = '0.0.3'
 
 default_app_config = 'learnupon_exporter.apps.LearnUponExporterAppConfig'  # pylint: disable=invalid-name
